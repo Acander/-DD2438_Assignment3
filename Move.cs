49a01372fad0cd980c58c7055e07f034a7d35122
Acander <adrianz@kth.se>
2020-04-20 14:55:03 +0200

    add Move class

---
+++ Move.cs
@@ -0,0 +1,20 @@
+﻿namespace Scrips
+{
+    public class Move
+    {
+        public float steeringAngle;
+        public float throttle;
+        public float footBrake;
+        public float handBrake;
+
+        public Move(float steeringAngle, float throttle, float footBrake, float handBrake)
+        {
+            this.steeringAngle = steeringAngle;
+            this.throttle = throttle;
+            this.footBrake = footBrake;
+            this.handBrake = handBrake;
+        }
+
+        
+    }
+}
